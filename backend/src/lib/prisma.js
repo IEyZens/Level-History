9c158c42eb437ab04298e7f956277658202dc649
IEyZens <roncint61@gmail.com>
2026-01-19 11:27:27 +0100

    Updates files

--- backend/src/lib/prisma.js
+++ backend/src/lib/prisma.js
@@ -1,12 +1,5 @@
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-import pg from "pg";
 
-const connectionString = process.env.DATABASE_URL;
-
-const pool = new pg.Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export default prisma;
