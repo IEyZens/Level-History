cd65447d2ac747506ad7dc11e8f00b806e0779be
IEyZens <roncint61@gmail.com>
2026-01-26 10:38:30 +0100

    Add swaggerUI

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,9 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
+import swaggerui from "swagger-ui-express";
+import yaml from "yamljs";
+
 import authRoutes from "./routes/auth.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import eventRoutes from "./routes/event.route.js";
@@ -15,6 +18,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+
+const swaggerDocument = yaml.load(path.join(__dirname, "../swagger.yaml"));
 
 app.use(
   cors({
@@ -28,6 +33,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+
+app.use("/api-docs", swaggerui.serve, swaggerui.setup(swaggerDocument));
 
 app.use("/auth", authRoutes);
 app.use("/events", eventRoutes);
